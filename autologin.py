8ee7c04855dfe89586129c8dd9ae51287affcd3d
zwx168238 <zwx168238@gmail.com>
2016-06-03 22:46:24 +0800

    rewrite userinfo with class
    rewrite userinfo with class and entry name and passwd

--- autologin.py
+++ autologin.py
@@ -9,30 +9,42 @@ import sqlite3
 import subprocess
 import sys
 import time
+import base64
 from argparse import ArgumentParser
 
 from BeautifulSoup import BeautifulSoup
 from selenium import webdriver
 
 web_addr = "https://github.com/login"
-name ="zwx168238@gmail.com"
-password ="1qaz!QAZ"
 
-def login():
+class userinfo(object):
+    def __init__(self, web_addr, name,password):
+        self.web_addr = web_addr
+        self.name = name
+        self.password = password
+
+def get_info():
+    name = base64.encodestring(raw_input("input username: "))
+    password = base64.encodestring(raw_input("input password: "))
+    info = userinfo(web_addr,base64.decodestring(name),base64.decodestring(password))
+
+def login(userinfo):
     driver = webdriver.Firefox()
-    driver.get(web_addr)
+    driver.get(info.web_addr)
     elem = driver.find_element_by_name("login")
-    elem.send_keys(name)
+    elem.send_keys(info.name)
     print name 
     elem =driver.find_element_by_name("password")
     print password
-    elem.send_keys(password)
+    elem.send_keys(info.password)
     elem = driver.find_element_by_name("commit")
     elem.click()
     time.sleep(5)
     driver.quit()
-
-
 if __name__ == "__main__":
-    login()
-
+    print "*************************************************"
+    name = base64.encodestring(raw_input("input username: "))
+    password = base64.encodestring(raw_input("input password: "))
+    info = userinfo(web_addr,base64.decodestring(name),base64.decodestring(password))
+    login(info)
+    print "*************************************************"
